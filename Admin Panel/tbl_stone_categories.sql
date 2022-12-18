be7c86acc86ec0c5f297ef04a2a9a7ecef84b830
Muhammad Abdullah <96911855+abdullahaslam2223@users.noreply.github.com>
2022-12-18 18:21:51 +0500

    fixing datatype of category name

--- Admin Panel/tbl_stone_categories.sql
+++ Admin Panel/tbl_stone_categories.sql
@@ -10,12 +10,10 @@ GO
 
 CREATE TABLE [dbo].[tbl_stone_categories](
 	[category_id] [int] IDENTITY(1,1) NOT NULL,
-	[category_name] [varbinary](50) NOT NULL,
+	[category_name] [varchar](50) NOT NULL,
  CONSTRAINT [PK_tbl_stone_categories] PRIMARY KEY CLUSTERED 
 (
 	[category_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-
